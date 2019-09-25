9fc621fc0295d7a698488320453348afc6d1a1aa
Richardio2002 <22622966+Richardio2002@users.noreply.github.com>
2019-09-25 16:13:59 -0400

    Add code for stone collection mechanism to teleop (version 1)
    Signed-off-by: Richardio2002 <22622966+Richardio2002@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_AtlanticCoast.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_AtlanticCoast.java
@@ -4,7 +4,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -19,13 +21,15 @@ public class FTC_AtlanticCoast extends LinearOpMode {
     //private DcMotor motor_3 = null;
     //private DcMotor motor_4 = null;
     private DcMotor armMotor = null;
+    //private CRServo stoneFlap = null;
+    private Servo stoneFlap = null;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        final double ARM_POWER = 0.2;
+        final double ARM_POWER = 0.5;
 
         //Motors
         motor_1 = hardwareMap.get(DcMotor.class, "motor_1");
@@ -41,6 +45,12 @@ public class FTC_AtlanticCoast extends LinearOpMode {
         //motor_4.setDirection(DcMotor.Direction.FORWARD);
         armMotor.setDirection(DcMotor.Direction.REVERSE);
 
+        //CR Servo
+        //stoneFlap = hardwareMap.get(CRServo.class, "stone_flap");
+        //stoneFlap.setDirection(CRServo.Direction.FORWARD);
+
+        //Servo
+        stoneFlap = hardwareMap.get(Servo.class, "stone_flap");
 
         //power
         double leftPower = 0;
@@ -48,6 +58,10 @@ public class FTC_AtlanticCoast extends LinearOpMode {
         //double motor3Power = 0;
         //double motor4Power = 0;
         double armPower = 0;
+
+        double stoneFlapPosition = 0.5;
+
+        stoneFlap.setPosition(stoneFlapPosition);
 
         waitForStart();
         runtime.reset();
@@ -65,7 +79,7 @@ public class FTC_AtlanticCoast extends LinearOpMode {
             } else if(-gamepad2.right_stick_y >= 0.2) {
                 armPower = -ARM_POWER;
             } else {
-                armPower = (-gamepad2.left_stick_y * 0.3);
+                armPower = (-gamepad2.left_stick_y * 0.6);
             }
             armPower = Range.clip(armPower, -1.0, 1.0);
 
@@ -80,16 +94,43 @@ public class FTC_AtlanticCoast extends LinearOpMode {
           //      motor_4.setPower(0);
           //  }
 
+            /*
+            //stoneFlap CRServo control
+            if (gamepad2.dpad_left) {
+                stoneFlap.setPower(-1.0);
+            } else if (gamepad2.dpad_right) {
+                stoneFlap.setPower(1.0);
+            } else {
+                stoneFlap.setPower(0.0);
+            }
+            */
+
+            //stoneFlap Servo control
+            if (gamepad2.dpad_left) {
+                stoneFlapPosition = stoneFlap.getPosition() - 0.05;
+            } else if (gamepad2.dpad_right) {
+                stoneFlapPosition = stoneFlap.getPosition() + 0.05;
+            }
+            while (gamepad2.x) {
+                stoneFlapPosition = stoneFlap.getPosition() - 0.05;
+            }
+            while (gamepad2.b) {
+                stoneFlapPosition = stoneFlap.getPosition() + 0.05;
+            }
+
             motor_1.setPower(leftPower);
             motor_2.setPower(rightPower);
             //motor_3.setPower(motor3Power);
             //motor_4.setPower(motor4Power);
             armMotor.setPower(armPower);
 
+            stoneFlap.setPosition(stoneFlapPosition);
+
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Drive Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.addData("Arm Motor(s)", "arm_motor (%.2f)", armPower);
+            telemetry.addData("Servo(s)", "stone_flap (%.2f)", stoneFlap.getPosition());
             telemetry.update();
         }
     }
