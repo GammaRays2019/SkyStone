34d6713b0ee8057f0ded2da66c672f47d7b84fd6
ghostshark547 <sharkboy1175@gmail.com>
2020-01-15 15:07:56 -0500

    Add GammaJR code
    Signed-off-by: ghostshark547 <sharkboy1175@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareGammaJR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HardwareGammaJR.java
@@ -2,19 +2,22 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class HardwareDummy {
+public class HardwareGammaJR {
     public ElapsedTime runtime = new ElapsedTime();
     public DcMotor rightDrive = null;
     public DcMotor leftDrive = null;
+    public Servo   stoneServo = null;
+    public Servo   foundServo = null;
 
     HardwareMap hwMap = null;
 
     private ElapsedTime period  = new ElapsedTime();
 
     // Constructor
-    public HardwareDummy () {
+    public HardwareGammaJR() {
 
     }
 
@@ -32,6 +35,9 @@ public class HardwareDummy {
 
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        stoneServo = hwMap.get(Servo.class, "stone_servo");
+        foundServo = hwMap.get(Servo.class, "found_servo");
     }
 
 }
